fa2cd393cb380945ddfb90be75374c82d55f45a1
Carlicia Smith <smith.carlicia@gmail.com>
2020-11-18 11:41:53 -0500

    html routes issue resolved

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,15 +1,13 @@
 const path = require("path");
 const router = require ("express").Router();
 
-module.exports = function() {
-    router.get("notes", function(req, res){
-        console.log("notes route hit");
-        res.sendFile(path.join(__dirname, "/public/notes.html"))
+
+    router.get("/notes", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/notes.html"))
     });
     
     router.get("*", function(req, res){
-        console.log("default route hit");
         res.sendFile(path.join(__dirname, "../public/index.html"))
     });
-    };
 
+module.exports = router;
