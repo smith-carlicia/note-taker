f051e4337ec76fefc10939d243383eafac685fdb
Carlicia Smith <smith.carlicia@gmail.com>
2020-11-18 11:21:16 -0500

    working to get html routes issue resolved

---
+++ routes/apiRoutes.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+const store = require("../db/store");
+const uuid = require("uuid");
+
+router.get("/api/notes", function(res, req) {
+    return res.json(store.readData());
+});
+
+router.post("/api/notes", (req, res) => {
+    const note = req.body;
+    note.id = uuid.v1();
+    db.push(note);
+    writeToDB(db);
+    return res.json(db);
+});
+
+router.delete("/api/notes:id", (res, req) => {
+    const id = req.params.id;
+    for (let i = 0; i < db.length; i++) {
+      if (db[i].id == id) {
+        db.splice(i, 1);
+        writeToDB(db);
+        res.json(db);
+        break;
+        };
+    };
+});
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,12 +1,13 @@
 const path = require("path");
 const router = require ("express").Router();
 
-router.get("notes", function(req, res){
-    res.sendFile(path.join(__dirname,"../public/notes.html"))
-});
+module.exports = function(router) {
+    router.get("notes", function(req, res){
+        res.sendFile(path.join(__dirname, "/public/notes.html"))
+    });
+    
+    router.get("*", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/index.html"))
+    });
+    };
 
-router.get("*", function(req, res){
-    res.sendFile(path.join(__dirname,"../public/index.html"))
-});
-
-module.exports = router;
