8516dffd91b41ea042778d24abbdcd344d46a56e
Carlicia Smith <smith.carlicia@gmail.com>
2020-11-19 02:19:25 -0500

    made changes to htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,7 +7,7 @@ const router = require ("express").Router();
     });
     
     router.get("/", function(req, res){
-        res.sendFile(path.join(__dirname, "../public/index.html"))
+        res.sendFile(path.join(__dirname, "/public/index.html"))
     });
 
 module.exports = router;
